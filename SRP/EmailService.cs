9d0dc66c25e909e1c1e85caf0f0a5059b8afb0a5
Makhanbay Yerassyl <makhanbayerasyl@gmail.com>
2026-02-08 20:48:45 +0500

    Create EmailService.cs

---
+++ SRP/EmailService.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class EmailService
+{
+    public void SendConfirmationEmail(string email)
+    {
+        Console.WriteLine("Confirmation email sent to: " + email);
+    }
+}
