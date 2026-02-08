9cfea86f882843e577fc605cb82c3b32f0008bc1
Makhanbay Yerassyl <makhanbayerasyl@gmail.com>
2026-02-08 20:48:35 +0500

    Create PaymentProcessor.cs

---
+++ SRP/PaymentProcessor.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class PaymentProcessor
+{
+    public void ProcessPayment(string paymentDetails)
+    {
+        Console.WriteLine("Payment processed using: " + paymentDetails);
+    }
+}
